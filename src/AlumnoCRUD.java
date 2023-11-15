3204221531bc2c1c6e0c4d545817256d8efcef24
Hugoag03 <hugoarevalogil08@gmail.com>
2023-11-15 12:14:27 +0100

    Añadido metodo bajaAlumno

--- src/AlumnoCRUD.java
+++ src/AlumnoCRUD.java
@@ -62,5 +62,16 @@ public class AlumnoCRUD {
         escribirAlumnos(listaAlumno);
 }
 
+public void bajaAlumno(String dni) {
+        ArrayList<Alumno> listaAlumno = leerAlumnos();
+        for (int i = 0; i < listaAlumno.size(); i++) {
+            if (listaAlumno.get(i).getDni().equalsIgnoreCase(dni)) {
+                listaAlumno.remove(i);
+            }
+        }
+        escribirAlumnos(listaAlumno);
+    
+    }
+
 
 }
